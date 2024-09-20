585ba1c8ccc4d9a44f3464af831197393fc0e399
Emrys Corbin <emrys.corbin@gmail.com>
2024-09-20 17:29:31 -0500

    Init

---
+++ src/test/java/msu/itc475/mwccdc/MwccdcApplicationTests.java
@@ -0,0 +1,13 @@
+package msu.itc475.mwccdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwccdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
